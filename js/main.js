2a063438853f34de9754fa847de2a4ffc9ead57f
Mohsine Amaach <mohsine.amaach@medium.ag>
2024-10-09 17:42:54 +0200

    funktions update

--- js/main.js
+++ js/main.js
@@ -25,25 +25,19 @@ document.getElementById('loginForm').addEventListener('submit', async function (
 
 
         localStorage.setItem('accessToken', accessToken);
-        localStorage.setItem('refreshToxken', refreshToken);
+        localStorage.setItem('refreshToken', refreshToken);
 
 
         document.getElementById('message').innerText = 'Erfolgreich eingeloggt!';
         document.getElementById('loginForm').style.display = 'none';
 
         await fetchAllUsers();
-
-
     } catch (error) {
         document.getElementById('message').innerText = 'Fehler: ' + error.message;
         console.error('Fehler:', error);
     }
 });
-
-
-
 async function fetchAllUsers() {
-
     const accessToken = localStorage.getItem('accessToken');
     const response = await fetch('https://market.fb-development.de/api/v1/users/?page=1&limit=10', {
         method: 'GET',
@@ -66,9 +60,71 @@ async function fetchAllUsers() {
             <td>${user.firstname} ${user.lastname}</td>
             <td>${user.email}</td>
             <td>${user.roles}</td>
+            <td>
+              <button onclick="editUser('${user.id}')">Bearbeiten</button>
+                <button onclick="deleteUser('${user.id}')">Löschen</button>
+            </td>
         `;
         usersBody.appendChild(row);
     });
+}
+async function editUser(userId) {
+    const accessToken = localStorage.getItem('accessToken');
+    const response = await fetch(`https://market.fb-development.de/api/v1/users/${userId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        }
+    });
+    const user = await response.json();
+    
+   
+    const modal = document.getElementById('editModal');
+    modal.style.display = 'block';
+    document.getElementById('editFirstName').value = user.firstname;
+    document.getElementById('editLastName').value = user.lastname;
+    document.getElementById('editEmail').value = user.email;
+    document.getElementById('editRoles').value = user.roles;
+    document.getElementById('saveChangesButton').onclick = async function() {
+        await saveUserChanges(userId);
+        alert('Die Bearbeitung war erfolgreich!');
+
+        document.getElementById('editModal').style.display = 'none';
+        location.reload();
+    };
+
+   
+
+}
+
+async function saveUserChanges(userId) {
+    const accessToken = localStorage.getItem('accessToken');
+
+    const rolesInput = document.getElementById('editRoles').value;
+    const rolesArray = rolesInput ? rolesInput.split(',').map(role => role.trim()) : [];
+
+    const updatedUser = {
+        firstname: document.getElementById('editFirstName').value,
+        lastname: document.getElementById('editLastName').value,
+        email: document.getElementById('editEmail').value,
+        roles: rolesArray 
+    };
+    
+     console.log(updatedUser);
+    const response = await fetch(`https://market.fb-development.de/api/v1/users/${userId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify(updatedUser)
+    });
+
+}
+
+function closeModal() {
+    document.getElementById('editModal').style.display = 'none';
 }
 
 async function updateToken() {
@@ -93,25 +149,23 @@ async function updateToken() {
         console.error('Fehler beim Aktualisieren des Tokens:', error);
     }
 }
+
 function isTokenExpired(token) {
+    if (!token) {
+        return true; 
+    }
     const payload = JSON.parse(atob(token.split('.')[1]));
     const expiryTime = payload.exp * 1000; 
     return Date.now() > expiryTime;
 }
 
 const accessToken = localStorage.getItem('accessToken');
-async function  checkAndUpdateToken()
-{
-
-    if (isTokenExpired(accessToken)) {
+async function checkAndUpdateToken() {
+    if (accessToken && isTokenExpired(accessToken)) {
         await updateToken();
     }
-} 
-
+}
  checkAndUpdateToken(); 
-
-
-
  document.addEventListener('DOMContentLoaded', async () => {
     await checkAndUpdateToken(); 
     const accessToken = localStorage.getItem('accessToken');
@@ -120,15 +174,93 @@ async function  checkAndUpdateToken()
        
         await fetchAllUsers();
         document.getElementById('loginForm').style.display = 'none'; 
-       
     }
 });
 
 
-$(window).on('scroll', function() {
-    if ($(window).scrollTop() > 50) {
-        $('.navbar').addClass('scrolled');
-    } else {
-        $('.navbar').removeClass('scrolled');
+async function deleteUser(userId) {
+    if (confirm('Möchten Sie diesen Benutzer wirklich löschen?')) {
+        const accessToken = localStorage.getItem('accessToken');
+        const response = await fetch(`https://market.fb-development.de/api/v1/users/${userId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        if (response.ok) {
+            alert('Benutzer erfolgreich gelöscht');
+            fetchAllUsers();  
+        } else {
+            alert('Fehler beim Löschen des Benutzers');
+        }
+    }
+}
+
+async function updateToken() {
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    try {
+        const response = await fetch('https://market.fb-development.de/api/v1/auth/refresh-access-token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ refreshToken })
+        });
+
+        if (!response.ok) {
+            throw new Error('Fehler beim Aktualisieren des Tokens');
+        }
+
+        const data = await response.json();
+        // Aktualisiere das Access-Token im localStorage
+        localStorage.setItem('accessToken', data.accessToken);
+    } catch (error) {
+        console.error('Fehler beim Aktualisieren des Tokens:', error);
+    }
+}
+
+document.getElementById('addUserForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const firstName = document.getElementById('addFirstName').value;
+    const lastName = document.getElementById('addLastName').value;
+    const email = document.getElementById('addEmail').value;
+    const rolesInput = document.getElementById('addRoles').value;
+    const rolesArray = rolesInput ? rolesInput.split(',').map(role => role.trim()) : [];
+
+    const newUser = {
+        firstname: firstName,
+        lastname: lastName,
+        email: email,
+        roles: rolesArray
+    };
+
+    try {
+        const accessToken = localStorage.getItem('accessToken');
+        const response = await fetch('https://market.fb-development.de/api/v1/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            },
+            body: JSON.stringify(newUser)
+        });
+
+        if (!response.ok) {
+            throw new Error('Fehler beim Hinzufügen des Benutzers');
+        }
+
+        // Benutzer erfolgreich hinzugefügt
+        document.getElementById('addMessage').innerText = 'Benutzer erfolgreich hinzugefügt!';
+        document.getElementById('addUserForm').reset(); // Formular zurücksetzen
+        $('#addUserModal').modal('hide'); // Modal schließen
+        fetchAllUsers(); // Alle Benutzer erneut laden
+
+    } catch (error) {
+        document.getElementById('addMessage').innerText = 'Fehler: ' + error.message;
+        console.error('Fehler:', error);
     }
 });
